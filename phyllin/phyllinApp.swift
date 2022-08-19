ac9fb772a0c4fe5a5451a10e2261f18a8780fbb1
faith <89432125+faithchongrui@users.noreply.github.com>
2022-08-19 13:44:10 +0800

    Initial Commit

---
+++ phyllin/phyllinApp.swift
@@ -0,0 +1,17 @@
+//
+//  phyllinApp.swift
+//  phyllin
+//
+//  Created by FAITH CHONG RUI EN stu on 19/8/22.
+//
+
+import SwiftUI
+
+@main
+struct phyllinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
