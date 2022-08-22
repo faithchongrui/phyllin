78db69e45618d868a8ce0f993cd580dac27d3f46
ndgsghdj <75611248+ndgsghdj@users.noreply.github.com>
2022-08-22 11:31:27 +0800

    yeah

--- phyllin/HomeView.swift
+++ phyllin/HomeView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var SearchQuery = ""
+    @State private var searchText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                TextField("What are you looking for?", text: $SearchQuery)
+            }
+            .navigationTitle("")
+        }
     }
 }
 
