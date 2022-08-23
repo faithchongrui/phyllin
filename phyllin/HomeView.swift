cd47b99ac79933c2340440c5d2f6da3c0b35ddd5
ndgsghdj <75611248+ndgsghdj@users.noreply.github.com>
2022-08-23 14:11:54 +0800

    Added ScrollView to HomeView

--- phyllin/HomeView.swift
+++ phyllin/HomeView.swift
@@ -13,7 +13,20 @@ struct HomeView: View {
     
     var body: some View {
         TabView {
-            Text("Home")
+            ScrollView {
+                VStack(spacing: 10) {
+                        ForEach(0..<10) {
+                            Text("Item \($0)")
+                                .foregroundColor(.white)
+                                .font(.largeTitle)
+                                .frame(width: 200, height: 200)
+                                .background(.red)
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
+            } //need to find a way to have more than one item in a row while still scrolling vertically
+            //how do view positions work, why is the scrollview in the middle qwq
+            .frame(height: 350)
                 .tabItem {
                     Image(systemName: "house.fill")
                     Text("Home")
