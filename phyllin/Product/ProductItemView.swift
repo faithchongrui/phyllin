3c14b7e75f855248247c1208c42732acec105a8c
ndgsghdj <75611248+ndgsghdj@users.noreply.github.com>
2022-08-26 08:34:36 +0800

    Added JSON Data for Products

---
+++ phyllin/Product/ProductItemView.swift
@@ -0,0 +1,20 @@
+//
+//  ProductItemView.swift
+//  phyllin
+//
+//  Created by Nikola Winata on 26/8/22.
+//
+
+import SwiftUI
+
+struct ProductItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductItemView()
+    }
+}
