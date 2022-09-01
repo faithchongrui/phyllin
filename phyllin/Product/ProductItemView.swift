bc975542ef094fed110238e7dec5324eedbe08c1
ndgsghdj <75611248+ndgsghdj@users.noreply.github.com>
2022-09-01 18:36:01 +0800

    JSON + design on item view, error in preview (possibly due to parsing in bundle extension file.)

--- phyllin/Product/ProductItemView.swift
+++ phyllin/Product/ProductItemView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct ProductItemView: View {
+    var product: Product
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 6) {
+            ZStack {
+                Image(product.image)
+                    .resizable()
+                    .scaledToFit()
+                    .padding(10)
+            }
+            .cornerRadius(12)
+        }
+        Text(product.name)
+            .font(.title3)
+            .fontWeight(.black)
+        
     }
 }
 
 struct ProductItemView_Previews: PreviewProvider {
     static var previews: some View {
-        ProductItemView()
+        ProductItemView(product: products[0])
+            .previewLayout(.fixed(width: 200, height: 200))
+            .padding()
     }
 }
