7f1bdf11a839db4ba1e3180e82ce7d8e7d06f174
faith <89432125+faithchongrui@users.noreply.github.com>
2022-09-04 21:21:25 +0800

    a

---
+++ phyllin/Utilities/ColourConstants.swift
@@ -0,0 +1,15 @@
+//
+//  ColourConstants.swift
+//  phyllin
+//
+//  Created by FAITH CHONG RUI EN stu on 20/8/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct Colours {
+    static let tealgreen: Color = .init("tealgreen")
+    static let cyanblue: Color = .init("cyanblue")
+    static let evergreen: Color = .init("evergreen")
+}
